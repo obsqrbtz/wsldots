ffdc42927efc3f00f1cdac164806a7294f996041
Daniel Dada <doesdeos@gmail.com>
2025-05-07 14:41:57 +0300

    added dotnet-sdk dep

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ git submodule init
 git submodule update --recursive
 
 sudo pacman -Sy --noconfirm
-sudo pacman -S --noconfirm stow nodejs npm dotnet-sdk-8.0 unzip zsh openssh cmake ninja clang zoxide lsd bat fzf fd ripgrep ranger wget git-delta mesa glu libxkbcommon fontconfig freetype2 ttf-dejavu
+sudo pacman -S --noconfirm stow nodejs npm dotnet-sdk dotnet-sdk-8.0 unzip zsh openssh cmake ninja clang zoxide lsd bat fzf fd ripgrep ranger wget git-delta mesa glu libxkbcommon fontconfig freetype2 ttf-dejavu
 sudo localectl set-locale LANG=en_US.UTF-8
 
 chsh -s /usr/bin/zsh "$(whoami)"
